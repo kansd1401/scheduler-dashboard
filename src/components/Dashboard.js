dba10697b4c0f8c34f8c5c8f8aaa87410b5f5b3c
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2019-12-05 12:37:12 -0800

    connected dasboard to the database

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,39 +2,59 @@ import React, { Component } from "react";
 import Loading from "components/Loading"
 import Panel from "components/Panel"
 import classnames from "classnames";
-
+import axios from "axios";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
-    focused: null
-  };
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
+   };
 
   componentDidMount() {
     const focused = JSON.parse(localStorage.getItem("focused"));
-
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
     if (focused) {
       this.setState({ focused });
     }
@@ -70,7 +90,7 @@ class Dashboard extends Component {
     <Panel
      key={panel.id}
      label={panel.label}
-     value={panel.value}
+     value={panel.getValue(this.state)}
      onClick={event => this.focus(panel.id)}
     />
    ));
